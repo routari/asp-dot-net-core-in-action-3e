c7361d56cb5ad5fae550bcd1de9e2936efcf97d4
Scott Chapman <eeekie@gmail.com>
2026-06-17 18:08:34 +0900

    Updated Program.cs for .NET 9+ standard template

--- Chapter18/A_CurrencyConverter/CurrencyConverter/Program.cs
+++ Chapter18/A_CurrencyConverter/CurrencyConverter/Program.cs
@@ -1,9 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
+#region Services
+
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.Configure<RouteOptions>(options =>
+{
+    options.LowercaseUrls = true;
+    options.AppendTrailingSlash = true;
+    options.LowercaseQueryStrings = true;
+});
+
+#endregion
 
 var app = builder.Build();
+
+#region Middleware
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -14,12 +26,32 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+#endregion
+
+#region Mapping
+
+// .NET 9+ static asset handling:
+// The key change is that static files are now represented as endpoints,
+// instead of middleware intercepting and shortcircuiting requests.
+// ASP.NET Core generates endpoint metadata during startup, eg:
+// Endpoint: /css/site.css
+// At build time ASP.NET Core now already knows which files exists.
+// No searching the filesystem each time. It's precomputed.
+// A further push towards AOT compilation.
+
+// Create static asset endpoint mappings
+app.MapStaticAssets(); // Now an endpoint, not a middleware
+
+app.MapRazorPages()
+    .WithStaticAssets(); // Tell Razor Pages to integrate with the new static asset system.
+
+#endregion
 
 app.Run();
